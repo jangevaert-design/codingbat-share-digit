aec11d35c617df3d9e900c9edfac21945cacb35a
Jan Gevaert <jan.gevaert@gmail.com>
2020-08-28 06:09:38 -0600

    Initial commit

---
+++ src/shareDigit.java
@@ -0,0 +1,6 @@
+public class shareDigit {
+  public boolean shareDigit(int a, int b) {
+    return ((a / 10) == (b / 10) || (a / 10) == (b % 10) || (a % 10) == (b / 10) ||
+        (a % 10)== (b % 10));
+  }
+}
